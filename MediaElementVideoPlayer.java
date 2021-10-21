84468caeb85022ec2bacb6824150125325f94daf
AliqanKDR <80212869+AliqanKDR@users.noreply.github.com>
2021-10-22 03:21:58 +0600

    Delete MediaElementVideoPlayer.java

--- MediaElementVideoPlayer.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class MediaElementVideoPlayer implements VideoPlayer {
-    @Override
-    public void playVideo() {
-        System.out.println("The video is being played by MediaElement");
-    }
-}
